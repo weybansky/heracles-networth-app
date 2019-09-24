63a4b97275d1479dfe23e1e58710eb338ca5aa22
sixfo <abiodun_flb@yahoo.com>
2019-09-24 02:34:48 +0100

    restructured and edited files

--- backend/connectdb.php
+++ backend/connectdb.php
@@ -1,5 +1,19 @@
 <?php
+session_start();
+	
+	$username = "";
+ 	$email = "";
+ 	$password = "";
+ 	$password = md5($password);
+ 	$errors = array();
 
+$db = mysqli_connect("us-cdbr-iron-east-02.cleardb.net","b0ae198915bb2d", "16a1a0d0", "heroku_6639abf7d3c0725");
+if(!$db){
+	die("unable to connect: " . mysqli_error($db));
+}
 
-
+ 
+ 	include ("signup-conn.php");   
+	include ("loginn-conn.php");  
+	include ("logout-conn.php");  
 ?>
