a028a8f0382c6b5b93405e3208ad158f07a8c37e
abdullah-laplace <abdullahakinwumi@g.gmail.com>
2019-09-26 09:02:00 +0100

    completed networth calculator and input validation

--- js/networth.js
+++ js/networth.js
@@ -1,5 +1,11 @@
 var sum = 0;
 var inputArray = [];
+
+var liabilitySum = 0;
+var liabilityInputArray = [];
+
+var networth = 0;
+
 function getArray(){
     var x = document.getElementsByClassName('asset');
     for(var i = 0; i < x.length ; i++ ){
@@ -27,6 +33,47 @@ function displaySum(sum){
     document.getElementById('assetSum').innerHTML = sum.toFixed(2);
 }
 
-function monitor(e) {
-    
+
+
+function getLiabilityArray(){
+    var y = document.getElementsByClassName('_liability');
+    for(var i = 0; i < y.length ; i++ ){
+        liabilityInputArray[i] = Number(y[i].value);
+    }
+    liabilityInputArray = liabilityInputArray
+    return liabilityInputArray;
+
+}
+function liabilityValidate(){
+    var liabilityArr = []
+    liabilityArr = getLiabilityArray();
+    var initliabilitySum=0;
+    for(var i = 0; i < liabilityArr.length ; i++ ){
+        console.log(liabilityArr[i]);
+        initliabilitySum += Number(liabilityArr[i]);
+        console.log("");
+    }
+    liabilitySum = initliabilitySum;
+    console.log(liabilitySum);
+    console.log("");
+    displayliabilitySum(liabilitySum);
+}
+function displayliabilitySum(liabilitySum){
+    document.getElementById('liabilitySum').innerHTML = '-' +liabilitySum.toFixed(2);
+}
+function displayNetworth(){
+    document.getElementById('_networth').innerHTML = (sum - liabilitySum).toFixed(2);
+}
+function _reset(){
+    document.getElementById('liabilitySum').innerHTML = 0 ;
+    document.getElementById('_networth').innerHTML = 0 ; 
+    document.getElementById('assetSum').innerHTML = 0;
+    var x = document.getElementsByClassName('asset');
+    for(var i = 0; i < x.length ; i++ ){
+        x[i].value = 0 ;
+    }
+    var y = document.getElementsByClassName('_liability');
+    for(var i = 0; i < y.length ; i++ ){
+    y[i].value = 0;
+}
 }
