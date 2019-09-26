08e83c3f932af8e0792f69773839dcd16c821680
PhilzAce <akuagwuphilemon11@email.com>
2019-09-26 13:14:51 +0100

    user Change Password

--- settings.php
+++ settings.php
@@ -8,6 +8,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title></title>
         <meta name="description" content="">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,800&display=swap" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="settings.css">
@@ -17,8 +19,7 @@
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
         <main class="container">
-            <header class="nav-header">
-                <span><a href="#"><img src="https://res.cloudinary.com/ottyhaq/image/upload/v1569400432/Heracles_Logo_1_m0sxzt.png" alt="Heracles"></a></span>
+            <header class="nav-header">                <span><a href="#"><img src="https://res.cloudinary.com/ottyhaq/image/upload/v1569400432/Heracles_Logo_1_m0sxzt.png" alt="Heracles"></a></span>
                 <span><a href="#"><img class="icon" src="https://res.cloudinary.com/ottyhaq/image/upload/v1569400431/Vector_6_fjie50.png" alt="username" width="30" height="30"></a>Username</span>
             </header>
             <div class="settings-contain">
@@ -38,15 +39,43 @@
                             </p>
                         </span>
                         <p>Profile Settings</p>
+<p class="">Change password</p>
                     </div>
                     <div class="settings-content">
-                        <img src="https://res.cloudinary.com/ottyhaq/image/upload/v1569403826/Rectangle_5_t4rgzw.png">  
+                    <form method="POST" action="includes/changePassword.php">
+                        <div class="form-group">
+                            <label for="InputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email">
+                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="InputPassword1">Password</label>
+                            <input type="password" class="form-control" id="InputPassword1"name="password" placeholder="Password">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="InputPassword2">New Password</label>
+                            <input type="password"name="newPassword" class="form-control" id="InputPassword2" placeholder="Password">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="InputPassword3">confirm new password</label>
+                            <input type="password" name="confirmNewPassword" class="form-control" id="InputPassword3" placeholder="Password">
+                        </div>
+                        <button type="submit"name="changePwd" class="btn btn-primary">Submit</button>
+                        </form>
+                    
+                        <!-- <img src="https://res.cloudinary.com/ottyhaq/image/upload/v1569403826/Rectangle_5_t4rgzw.png">   -->
                     </div>
                     <div class="logout">
                         <a href="#"><img src="https://res.cloudinary.com/benjee/image/upload/v1569349789/Group_18.1_mddzax.svg" alt="Heracles"></a>
                     </div>
                 </section>  
         </main>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
         <script src="" async defer></script>
     </body>
 </html>
