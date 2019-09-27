510497689df96bea04bf3b0b2442ee8722fd98ab
abdullah-laplace <abdullahakinwumi@g.gmail.com>
2019-09-27 16:21:54 +0100

    fixed responsiveness for dashboard

--- js/networth.js
+++ js/networth.js
@@ -20,13 +20,13 @@ function inputValidate(){
     arr = getArray();
     var initsum=0;
     for(var i = 0; i < arr.length ; i++ ){
-        console.log(arr[i]);
+        // console.log(arr[i]);
         initsum += Number(arr[i]);
-        console.log("");
+        // console.log("");
     }
     sum = initsum;
-    console.log(sum);
-    console.log("");
+    // console.log(sum);
+    // console.log("");
     displaySum(sum);
 }
 function displaySum(sum){
@@ -49,13 +49,13 @@ function liabilityValidate(){
     liabilityArr = getLiabilityArray();
     var initliabilitySum=0;
     for(var i = 0; i < liabilityArr.length ; i++ ){
-        console.log(liabilityArr[i]);
+        // console.log(liabilityArr[i]);
         initliabilitySum += Number(liabilityArr[i]);
-        console.log("");
+        // console.log("");
     }
     liabilitySum = initliabilitySum;
-    console.log(liabilitySum);
-    console.log("");
+    // console.log(liabilitySum);
+    // console.log("");
     displayliabilitySum(liabilitySum);
 }
 function displayliabilitySum(liabilitySum){
@@ -65,15 +65,22 @@ function displayNetworth(){
     document.getElementById('_networth').innerHTML = (sum - liabilitySum).toFixed(2);
 }
 function _reset(){
-    document.getElementById('liabilitySum').innerHTML = 0 ;
-    document.getElementById('_networth').innerHTML = 0 ; 
-    document.getElementById('assetSum').innerHTML = 0;
+    document.getElementById('liabilitySum').innerHTML = '' ;
+    document.getElementById('_networth').innerHTML = '' ; 
+    document.getElementById('assetSum').innerHTML = '';
     var x = document.getElementsByClassName('asset');
     for(var i = 0; i < x.length ; i++ ){
-        x[i].value = 0 ;
+        x[i].value = '' ;
     }
     var y = document.getElementsByClassName('_liability');
     for(var i = 0; i < y.length ; i++ ){
-    y[i].value = 0;
+    y[i].value = '';
+    }
 }
+
+function callName(){
+    setTimeout(hide, 3000);
+}
+function hide(){
+    document.getElementById('animate_name').style.visibility = 'hidden';
 }
