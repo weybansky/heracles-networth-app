5376ea761b0d3fbc3662bb90d1dfce15d0faf1bf
sixfo <abiodun_flb@yahoo.com>
2019-10-03 18:11:46 +0100

    validated signup page with php and also styled error displays
    I added a server.php file to validate the signup page and excluded the j.s file first used...
    I also styled the error display on the signup page...

---
+++ server.php
@@ -0,0 +1,82 @@
+<?php
+session_start();
+
+// initializing variables
+$fullname = "";
+$email    = "";
+$password = "";
+$confirmPassword = "";
+$mobile = "";
+$errors = array(); 
+
+// connect to the database
+define('DB_SERVER', "us-cdbr-iron-east-02.cleardb.net");
+define('DB_USER',"b0ae198915bb2d");
+define('DB_PASS' ,"16a1a0d0");
+define('DB_NAME', "heroku_6639abf7d3c0725");
+
+$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }
+
+
+// REGISTER USER
+if (isset($_POST['regBtn'])) {
+  $fullname = $_POST['fullname'];
+  $email = $_POST['email'];
+  $phone = $_POST['mobile'];
+  $password = $_POST['password'];
+  $confirmPassword = $_POST['confirmPassword'];
+
+  if (empty($fullname)) { array_push($errors, "First Name cannot be blank");  }
+    if (empty($email)) { array_push($errors, "Email cannot be blank"); }
+    if (empty($phone)) { array_push($errors, "Phone Number cannot be blank"); }
+    if (empty($password)) { array_push($errors, "Password cannot be blank"); }
+    if ($password != $confirmPassword) {
+  array_push($errors, "The two passwords do not match");
+    }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)){ 
+    array_push($errors, "Invalid Email");
+  }
+  if (!preg_match("/^[a-zA-Z ]*$/",$fullname)) {
+      array_push($errors, "Only letters and white space allowed for firstname"); 
+    }
+
+    if(!preg_match("/^[0-9]*$/", $phone) || strlen($phone) != 11){
+      array_push($errors, "Nigeria Number Only e.g 09012345678");
+    }
+
+    if(strlen($fullname) < 5){
+      array_push($errors, "Fullname cannot be less than 5 characters");
+    }
+    if(strlen($password) < 6){
+      array_push($errors, "Password cannot be less than 6 characters");
+    }
+
+
+  $user_check_query = "SELECT * FROM users WHERE email='$email' LIMIT 1";
+  $result = mysqli_query($con, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+
+    if ($user) { // if user exists
+      if ($user['email'] === $email) {
+      array_push($errors, "email already exists");
+    }
+  }
+
+  if (count($errors) == 0) {
+    $password = md5($password);//encrypt the password before saving in the database
+
+    $query = "INSERT INTO users (fullname, email, phone, password) 
+          VALUES('$fullname', '$email', '$phone', '$password')";
+    mysqli_query($con, $query);
+    $_SESSION['fullname'] = $fullname;
+    $_SESSION['success'] = "Registeration successful, please login";
+    header('Location: signin.php');
+  }
+
+}
--- signup.php
+++ signup.php
@@ -1,3 +1,4 @@
+<?php require_once ("server.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,40 +18,12 @@
                  
                     <div class="signup-form animated slideInLeft">
                     
-                         <?php
-                            if (isset($_GET["error"])) {
-                               if ($_GET['error']=='userAlreadyExist') {
-                                    echo '<p><p style = "text-align:center" class="text-danger"> User already exists</p><p>';
-                                   }elseif($_GET['error']=='emptyfields'){
-                                    echo '<p><p style = "text-align:center" class="text-danger"> PLease fill all fields correctly</p><p>';
-                                   }
-                                   
-                                   elseif($_GET['error']=='pwd'){
-                                    echo '<p><p style = "text-align:center" class="text-danger"> please confirm password</p><p>';
-                                   }
-                                   
-                                   elseif($_GET['error']=='num'){
-                                    echo '<p><p style = "text-align:center" class="text-danger"> Check your Number</p><p>';
-                                   }
-                                   
-                                   elseif($_GET['error']=='emails'){
-                                    echo '<p><p style = "text-align:center" class="text-danger"> Check your Email</p><p>';
-                                   }
-
-                                   
-                                   elseif($_GET['error']=='name'){
-                                    echo '<p><p style = "text-align:center" class="text-danger"> Check your name</p><p>';
-                                   }
-
-                                   else{
-                                    echo '<p><p style = "text-align:center" class="text-danger">Something is not right</p><p>';
-                                   }    
-                                }
-                           ?> 
-                         <form action="includes/signup.inc.php" method="POST" name="form" onsubmit="return formValidation()">
+                   
+                         <form action="signup.php" method="POST" name="form">
                             <div class="form-header">
                                     <img class="heracles-logo" src="https://res.cloudinary.com/benjee/image/upload/v1569459183/Heracles_Logo_2_qve8nw.svg" alt="Heracles">    
                                     <h1>Create Your Account</h1>
+                                    <?php include ("error.php"); ?>
                                 </div>
                             <div class="form-group">
                                 <input type="text" class="login-control" id="fullname" placeholder="Fullname"  name="fullname" required>
@@ -85,6 +58,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script type ='text/javascript' src = 'js/signup.js'></script>
+    <!-- <script type ='text/javascript' src = 'js/signup.js'></script> -->
 </body>
 </html>
