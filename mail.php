1d7ee03816ac5889dedebbcc08956d789f39abfd
sixfo <abiodun_flb@yahoo.com>
2019-10-04 18:11:12 +0100

    updated server.php and mail.php

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@
 
         // access
         $secretKey = '6LcUkrsUAAAAAD4eaFqnbz1HaQjc5rwUjGBB7i8_';
-        // $captcha = $_POST['g-recaptcha-response'];
+        $captcha = $_POST['g-recaptcha-response'];
 
         // if(!$captcha){
         //   echo '<p class="alert alert-warning">Please check the the captcha form.</p>';
